569beca4bc36b07e724df4c57fac258ffad16509
Eric Cloninger <ericc@pobox.com>
2026-08-27 13:21:18 -0500

    Brought over more changes from FTCH project

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -31,13 +31,14 @@ $(document).ready(function() {
         query.split(lunr.tokenizer.separator).forEach(function (term) {
           q.term(term, { boost: 100, presence: lunr.Query.presence.REQUIRED } )
           if(query.lastIndexOf(" ") != query.length-1){
-            q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10, presence: lunr.Query.presence.REQUIRED })
+            q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
           }
           if (term != ""){
-            q.term(term, {  usePipeline: false, editDistance: 1, boost: 1, presence: lunr.Query.presence.REQUIRED })
+            q.term(term, {  usePipeline: false, editDistance: 1, boost: 1 })
           }
         })
       });
+    console.log(result)
     resultdiv.empty();
     resultdiv.prepend('<p class="results__found">'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
     for (var item in result) {
